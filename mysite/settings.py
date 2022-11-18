f62b01da03362cbbfa99665db54fa287a41e6269
Saif <92519922+SA12IF34@users.noreply.github.com>
2022-11-18 21:13:37 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,9 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'assets/'
-STATIC_ROOT = 'assets/'
+# STATIC_ROOT = 'assets/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'frontend/dist/assets'
+    BASE_DIR / 'frontend/dist/assets',
+    BASE_DIR / 'assets'
 ]
 
 
